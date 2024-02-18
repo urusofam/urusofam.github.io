cd975cb509461b9407664668a13b7ba9dec055a1
urusofam <urusofam@gmail.com>
2024-02-18 14:24:30 +0300

    v1.02

--- java/main.js
+++ java/main.js
@@ -1,5 +1,5 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { STLLoader } from '../loaders/STLLoader.js';
+import * as THREE from '../build/three.module.js';
+import { STLLoader } from '../build/STLLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
